d025df71f3e908c4252b864aee65e5ee2c7dd466
princejogani <joganiprince1@gmail.com>
2026-04-15 17:05:17 +0530

    feat: update Puppeteer cache directory configuration in .puppeteerrc.cjs and render.yaml

--- backend/.puppeteerrc.cjs
+++ backend/.puppeteerrc.cjs
@@ -1,8 +1,3 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
 module.exports = {
-    cacheDirectory: process.env.PUPPETEER_CACHE_DIR || join(__dirname, '.cache', 'puppeteer'),
+    cacheDirectory: '/opt/render/.cache/puppeteer',
 };
